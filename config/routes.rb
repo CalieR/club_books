888926ff9e7cfa0014f7d1d0f8b459faa5c3fcc9
CalieR <calielrushton@gmail.com>
2019-03-25 14:02:13 +0000

    added seed data and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :meetings
-  resources :books
-  resources :memberships
-  resources :users
+  resources :meetings, except: [:show, :index] 
+  resources :books, only: [:show, :index]
+  resources :memberships, only: [:create, :update, :delete]
+  resources :users, except: [:edit, :update, :index]
   resources :clubs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
