461d28aedcb2376aab8e70e3fae847f3d4afb3e7
max-powell <github email address>
2019-03-25 16:18:30 +0000

    Built users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,36 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :destroy]
+
   def show
-    @user = User.find(session[:user_id])
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.create(user_params)
+    if @user.valid?
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      flash[:errors] = @user.errors.full_messages
+      redirect_to new_user_path
+    end
   end
 
   def destroy
+    @user.destroy
+    redirect_to login_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
+
+  def set_user
+    @user = User.find(session[:user_id])
   end
 end
