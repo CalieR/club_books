01afad8b6aedcdb6968e4264210ed58198c5ad47
max-powell <github email address>
2019-03-26 13:44:48 +0000

    users can join clubs and create their own

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -1,14 +1,31 @@
 class ClubsController < ApplicationController
 
-    def index 
-        @clubs = Club.all
-    end
+  def index
+      @user = User.find(session[:user_id])
+      @clubs = Club.all
+  end
 
-    def show 
-        @user = User.find(session[:user_id])
-        @club = Club.find(params[:id])
-        redirect_to @user unless @user.club_ids.include?(@club.id)
-    end
+  def show
+      @user = User.find(session[:user_id])
+      @club = Club.find(params[:id])
+      redirect_to @user unless @user.club_ids.include?(@club.id)
+  end
 
-   
+  def new
+    @club = Club.new
+  end
+
+  def create
+    @user = User.find(session[:user_id])
+    @club = Club.create(club_params)
+    @club.first_user = @user
+    redirect_to @club
+  end
+
+  private
+
+  def club_params
+    params.require(:club).permit(:name, :description)
+  end
+
 end
--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,2 +1,8 @@
 class MembershipsController < ApplicationController
+  def create
+    @user = User.find(session[:user_id])
+    @club = Club.find(params[:membership][:club_id])
+    @club.users << @user
+    redirect_to @club
+  end
 end
